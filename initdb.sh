401a4d7c35454c64f241c3c120913d5979cb27d5
dev-kara <aa@bb.cc>
2016-11-28 11:15:40 +0900

    add row query

--- initdb.sh
+++ initdb.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
 sudo -u postgres psql -c "create database test1;create database test2;"
 sudo -u postgres psql -d test1 -c "create table t_test(id serial, name text);"
 sudo -u postgres psql -d test2 -c "create table t_test(id serial, name text);"
+sudo -u postgres psql -d test2 -c "insert into t_test(name) values('a'),('b'),('c')";
